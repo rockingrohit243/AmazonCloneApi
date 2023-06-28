f86f45bd1309dcc99ea076fccc3c0496de628d31
rohit <rockingrohit243@gmail.com>
2023-06-28 16:47:00 +0530

    First commit

---
+++ src/main/java/com/java/flipkartcloneapi/FlipkartCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.java.flipkartcloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartCloneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipkartCloneApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/flipkartcloneapi/FlipkartCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.flipkartcloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartCloneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
